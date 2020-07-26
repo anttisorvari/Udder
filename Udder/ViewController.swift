147952f136f85c6b746e3b611dd4752843168c39
Antti Sorvari <antti.sorvari@gmail.com>
2020-07-26 19:06:30 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Sorvari, Antti on 26.7.2020.
+//  Copyright © 2020 Antti Sorvari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
